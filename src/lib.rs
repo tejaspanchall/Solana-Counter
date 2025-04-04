6c12b9db7e11fce389e5a0cee625b3903af398f4
tejaspanchall <tejaspanchal127@gmail.com>
2025-04-04 16:16:25 +0530

    Initial

---
+++ src/lib.rs
@@ -0,0 +1,46 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+    entrypoint,
+    program_error::ProgramError
+};
+
+entrypoint!(process_instruction);
+
+#[derive(BorshSerialize, BorshDeserialize)]
+struct Counter {
+    count: u32,
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+enum CounterInstruction {
+    Increment(u32),
+    Decrement(u32),
+}
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let account = &accounts.iter().next().ok_or(ProgramError::NotEnoughAccountKeys)?;
+    let mut counter: Counter = Counter::try_from_slice(&account.data.borrow())?;
+
+    match CounterInstruction::try_from_slice(instruction_data)? {
+        CounterInstruction::Increment(amount: u32) => {
+            counter.count += amount;
+        }
+        CounterInstruction::Decrement(amount: u32) => {
+            counter.count -= amount;
+        }
+    }
+
+    counter.serialize(&mut &mut account.data.borrow_mut()[..])?;
+    
+    msg!("Counter updated to {}", counter.count);
+    
+    Ok(())
+}
