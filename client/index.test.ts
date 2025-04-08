43ad4aa08001855e89414feb73ce13293495fc79
tejaspanchall <tejaspanchal127@gmail.com>
2025-04-08 16:21:01 +0530

    updated test.ts

--- client/index.test.ts
+++ client/index.test.ts
@@ -1,4 +1,5 @@
 import * as borsh from "borsh";
+import { Keypair, Connection, PublicKey, Transaction, SystemProgram, LAMPORTS_PER_SOL, TransactionInstruction } from "@solana/web3.js";
 
 class CounterAccount {
     count = 0;
@@ -15,3 +16,51 @@ const GREETING_SIZE = borsh.serialize(
     new CounterAccount({count: 0})
 ).length;
 
+let counterAccountKeypair: Keypair;
+let adminKeypair: Keypair;
+
+test("counter does increase", async () => {
+    adminKeypair = Keypair.generate();
+    counterAccountKeypair = new Keypair();
+
+    const connection = new Connection("http://localhost:8899", "confirmed");
+    const res = await connection.requestAirdrop(adminKeypair.publicKey, 2 * LAMPORTS_PER_SOL);
+    await connection.confirmTransaction(res);
+
+    const programId = new PublicKey("AmRPAbSzoeQqF76oXAuXevrSVa5CmIbeVj6L9AY1SuU");
+    const lamports = await connection.getMinimumBalanceForRentExemption(
+        GREETING_SIZE
+    );
+
+    const createCounterAccIx = SystemProgram.createAccount({
+        fromPubkey: adminKeypair.publicKey,
+        lamports,
+        newAccountPubkey: counterAccountKeypair.publicKey,
+        programId: programId,
+        space: GREETING_SIZE,
+    });
+
+    const tx = new Transaction();
+
+    tx.add(new TransactionInstruction({
+        keys: [{
+            pubkey: counterAccountKeypair.publicKey,
+            isSigner: false,
+            isWritable: true,
+        }],
+        programId: programId,
+        data: Buffer.from(new Uint8Array([0, 1, 0, 0, 0])),
+    }));
+
+    const txHash = await connection.sendTransaction(tx, [adminKeypair, counterAccountKeypair]);
+    await connection.confirmTransaction(txHash);
+    console.log(txHash);
+
+    const counterAccount = await connection.getAccountInfo(counterAccountKeypair.publicKey);
+    if (!counterAccount) {
+        throw new Error("Counter account not found");
+    }
+    const counter = borsh.deserialize(schema, counterAccount.data) as CounterAccount;
+    console.log(counter.count);
+    expect(counter.count).toBe(0);
+});
